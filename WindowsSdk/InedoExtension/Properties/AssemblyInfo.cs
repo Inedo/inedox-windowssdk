8950827a3fa5477176a35fa10fd791c150ad56f8
Greg D <gdivis@inedo.com>
2019-09-03 22:02:14 -0400

    add appropriate AppliesTo attribute

--- WindowsSdk/InedoExtension/Properties/AssemblyInfo.cs
+++ WindowsSdk/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("Windows SDK")]
 [assembly: AssemblyDescription("Contains actions to build software targeting Windows or .NET platforms, including Build Project, MSBuild, Click Once, etc.")]
@@ -12,3 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(false)]
+
+[assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Otter)]
