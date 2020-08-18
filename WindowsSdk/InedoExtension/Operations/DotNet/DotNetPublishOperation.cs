b16391980a4f36c7ee51df5cf4e9b016767cd0c6
Greg D <gdivis@inedo.com>
2020-08-18 13:16:58 -0400

    Add DotNet::Publish operation. Closes #9

---
+++ WindowsSdk/InedoExtension/Operations/DotNet/DotNetPublishOperation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Inedo.Documentation;
+using Inedo.Extensibility;
+
+namespace Inedo.Extensions.WindowsSdk.Operations.DotNet
+{
+    [DisplayName("dotnet publish")]
+    [ScriptAlias("Publish")]
+    [Description("Builds a .NET Core/Framework/Standard project using dotnet publish.")]
+    [Example(@"# Publish ~\src\MyProject.csproj with Release configuration for .net core 3.1, restoring NuGet packages from the InternalNuGet source
+DotNet::Publish ~\src\MyProject.csproj
+(
+    Configuration: Release,
+    Framework: netcoreapp3.1,
+    PackageSource: InternalNuGet
+);")]
+    [SeeAlso(typeof(DotNetBuildOperation))]
+    public sealed class DotNetPublishOperation : DotNetOperation
+    {
+        protected override string CommandName => "publish";
+    }
+}
---
+++ WindowsSdk/InedoExtension/Operations/DotNet/DotNetVerbosityLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace Inedo.Extensions.WindowsSdk.Operations.DotNet
+{
+    public enum DotNetVerbosityLevel
+    {
+        Quiet,
+        Minimal,
+        Normal,
+        Detailed,
+        Diagnostic
+    }
+}
